02e919964ab562787d012e69e352dd6c0c6a47ed
ruaanvds <rrvds87@gmail.com>
2018-12-30 01:54:31 -0800

    Progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_compass/flutter_compass.dart';
-import 'dart:math' as math;
+import 'package:witw/views/landing.dart';
 
-import 'maps.dart';
+void main() => runApp(WITWApp());
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  double _direction;
-
-  @override
-  void initState() {
-    super.initState();
-    FlutterCompass.events.listen((double direction) {
-      setState(() {
-        _direction = direction;
-      });
-
-      getBearing(-33.344921, 18.161560, 51.5007292, -0.1246254);
-    });
-  }
-
+class WITWApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: new Scaffold(
-        body: new Container(
-            alignment: Alignment.center,
-            color: Colors.white,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        child: Text(
-                          '$_direction',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      new Transform.rotate(
-                        angle: ((_direction ?? 0) * (math.pi / 180) * -1),
-                        child: new Image.asset('assets/compass.png'),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            )),
+      title: 'Animation Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: new LandingView(title: "Animation Main Page"),
     );
   }
 }
---
+++ lib/views/landing.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:witw/views/compass.dart';
+
+class LandingView extends StatelessWidget {
+  LandingView({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    // Create title widget
+    Widget titleSection = Container(
+        child: Text('...where in the world?',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Colors.black54,
+                fontSize: 30.0,
+                fontWeight: FontWeight.w900)));
+
+    // Create buttons
+    Widget button = RaisedButton(
+      color: Colors.white,
+      child: Text('Start'),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => EasingAnimationWidget()),
+        );
+      },
+    );
+
+    return new Material(
+        child: Container(
+            alignment: Alignment(0.0, 0.0),
+            color: Colors.blue,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'assets/cp-red.png',
+                  width: 200.0,
+                  height: 200.0,
+                ),
+                titleSection,
+                button
+              ],
+            )));
+  }
+}
